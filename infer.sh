fe49cd12fd38ef0577ef1235a188ee1541cb0507
Isen Naiken <isen.naiken@nexomedis.com>
2026-02-22 09:59:37 +0100

    ajout infer.sh

---
+++ infer.sh
@@ -0,0 +1,13 @@
+#!/bin/ksh 
+#$ -q gpu
+#$ -o result.out
+#$ -j y
+#$ -N schrodinger
+cd $WORKDIR
+cd /beegfs/data/work/imvia/in156281/diffusion_schrodinger_bridge
+source /beegfs/data/work/imvia/in156281/diffusion_schrodinger_bridge/venv/bin/activate
+module load python
+export PYTHONPATH=/work/imvia/in156281/diffusion_schrodinger_bridge/venv/lib/python3.9/site-packages:$PYTHONPATH
+export MPLCONFIGDIR=/work/imvia/in156281/.cache/matplotlib
+
+python infer.py
